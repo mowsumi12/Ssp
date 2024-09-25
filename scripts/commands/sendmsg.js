eef5ceb190bb70fff31a9759f4aae3bcd6856f23
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-09-25 10:05:07 +0600

    Update sendmsg.js

--- scripts/commands/sendmsg.js
+++ scripts/commands/sendmsg.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	version: "0.0.2",
 	permission: 2,
   prefix: true,
-	credits: "Nayan",
+	credits: "nazrul",
 	description: "sendmsg",
 	category: "admin",
 	usages: "sendmsg [user]/[thread] id msg",
