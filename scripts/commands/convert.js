200ba397b24c5adb134bb59ac74d0fdedff839cd
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-09-25 10:16:13 +0600

    Update convert.js

--- scripts/commands/convert.js
+++ scripts/commands/convert.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "convert",
     version: "1.0.0",
     permission: 0,
-    credits: "Nayan",
+    credits: "nazrul",
     prefix: true,
     description: "",
     category: "user",
