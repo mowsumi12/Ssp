11549239e6984f8d7af8eb19b9be7fd0d4bd1952
ritu-mikail <mdmikail673@gmail.com>
2024-10-02 11:02:14 +0600

    Update hack.js

--- scripts/commands/hack.js
+++ scripts/commands/hack.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "hack", 
   version: "1.0.0", 
   permission: 0,
-  credits: "Nayan",
+  credits: "Nazarul",
   description: "example",
   prefix: true,
   category: "Fun", 
