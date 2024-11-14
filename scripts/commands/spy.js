27eb53a283168c29bc7b5939ee51b55890f08574
ritu-mikail <mdmikail673@gmail.com>
2024-11-14 16:13:14 +0600

    Update spy.js

--- scripts/commands/spy.js
+++ scripts/commands/spy.js
@@ -1,29 +1,38 @@
+const axios = require("axios");
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
+
 module.exports = {
   config: {
     name: "spy",
     version: "1.0",
-    credits: "nazrul",
-    countDown: 60,
     permission: 0,
-    prefix:true,
-    description: "Get user information and avatar",
-    usages: "[@mention]",
-    longDescription: "Get user information and avatar by mentioning",
-    category: "image",
+    prefix: true,
+    credits: "Dipto",
+    description: "Get user information and profile photo",
+    category: "information",
+    cooldowns: 10,
   },
 
-   run: async function ({ event, message, usersData, api, args, getLang }) {
-    let avt;
+  run: async function ({
+    event,
+    Users,
+    api,
+    args,
+  }) {
     const uid1 = event.senderID;
+
     const uid2 = Object.keys(event.mentions)[0];
     let uid;
 
     if (args[0]) {
-      // Check if the argument is a numeric UID
       if (/^\d+$/.test(args[0])) {
         uid = args[0];
       } else {
-        // Check if the argument is a profile link
         const match = args[0].match(/profile\.php\?id=(\d+)/);
         if (match) {
           uid = match[1];
@@ -32,37 +41,69 @@ module.exports = {
     }
 
     if (!uid) {
-      // If no UID was extracted from the argument, use the default logic
-      uid = event.type === "message_reply" ? event.messageReply.senderID : uid2 || uid1;
+      uid =
+        event.type === "message_reply"
+          ? event.messageReply.senderID
+          : uid2 || uid1;
+    }
+    const response = await axios.get(
+      `${await baseApiUrl()}/baby?list=all`
+    );
+    const dataa = response.data || { teacher: { teacherList: [] } };
+    let babyTeach = 0;
+
+    if (dataa?.teacher?.teacherList?.length) {
+      babyTeach = dataa.teacher.teacherList.find((t) => t[uid])?.[uid] || 0;
     }
 
-    api.getUserInfo(uid, async (err, userInfo) => {
-      if (err) {
-        return message.reply("Failed to retrieve user information.");
-      }
+    const userInfo = await api.getUserInfo(uid);
+    const avatarUrl = `https://graph.facebook.com/${uid}/picture?height=1500&width=1500&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
 
-      const avatarUrl = await usersData.getAvatarUrl(uid);
+    let genderText;
+    switch (userInfo[uid].gender) {
+      case 1:
+        genderText = "𝙶𝚒𝚛𝚕🙋🏻‍♀️";
+        break;
+      case 2:
+        genderText = "Boy🙋🏻‍♂️";
+        break;
+      default:
+        genderText = "𝙶𝚊𝚢🤷🏻‍♂️";
+    }
 
-      // Gender mapping
-      let genderText;
-      switch (userInfo[uid].gender) {
-        case 1:
-          genderText = "Girl";
-          break;
-        case 2:
-          genderText = "Boy";
-          break;
-        default:
-          genderText = "Unknown";
-      }
+    const money = (await Users.get(uid)).money;
+const allUser = await Users.getAll(), rank = allUser.slice().sort((a, b) => b.exp - a.exp).findIndex(user => user.userID === uid) + 1, moneyRank = allUser.slice().sort((a, b) => b.money - a.money).findIndex(user => user.userID === uid) + 1;
 
-      // Construct and send the user's information with avatar
-      const userInformation = `❏ Name: ${userInfo[uid].name}\n❏ Profile URL: ${userInfo[uid].profileUrl}\n❏ Gender: ${genderText}\n❏ User Type: ${userInfo[uid].type}\n❏ Is Friend: ${userInfo[uid].isFriend ? "Yes" : "No"}\n❏ Is Birthday today: ${userInfo[uid].isBirthday ? "Yes" : "No"}`;
+    const position = userInfo[uid].type;
 
-      message.reply({
-        body: userInformation,
-        attachment: await global.utils.getStreamFromURL(avatarUrl)
-      });
-    });
-  }
+    const userInformation = `
+╭─[ 𝐔𝐒𝐄𝐑 𝐈𝐍𝐅𝐎 ]
+├‣ 𝙽𝚊𝚖𝚎: ${userInfo[uid].name}
+├‣ 𝙽𝚒𝚌𝚔𝙽𝚊𝚖𝚎: ${userInfo[uid].alternateName || "𝙽𝚘𝚗𝚎"}
+├‣ 𝚄𝙸𝙳: ${uid}
+├‣ 𝙲𝚕𝚊𝚜𝚜: ${position ? position?.toUpperCase() : "𝙽𝚘𝚛𝚖𝚊𝚕 𝚄𝚜𝚎𝚛🥺"}
+├‣ 𝚄𝚜𝚎𝚛𝚗𝚊𝚖𝚎: ${userInfo[uid].vanity ? userInfo[uid].vanity : "𝙽𝚘𝚗𝚎"}
+├‣ 𝙶𝚎𝚗𝚍𝚎𝚛: ${genderText}
+├‣ 𝙱𝚒𝚛𝚝𝚑𝚍𝚊𝚢: ${userInfo[uid].isBirthday !== false ? userInfo[uid].isBirthday : "𝙿𝚛𝚒𝚟𝚊𝚝𝚎"}
+├‣ 𝙵𝚛𝚒𝚎𝚗𝚍 𝚠𝚒𝚝𝚑 𝚋𝚘𝚝: ${userInfo[uid].isFriend ? "𝚈𝚎𝚜✅" : "𝙽𝚘❎"}
+╰‣ 𝙿𝚛𝚘𝚏𝚒𝚕𝚎 𝚄𝚁𝙻: ${userInfo[uid].profileUrl}
+
+╭─[ 𝐔𝐒𝐄𝐑 𝐒𝐓𝐀𝐓𝐒 ]
+├‣ 𝙼𝚘𝚗𝚎𝚢: $${formatMoney(money)}
+├‣ 𝚁𝚊𝚗𝚔: #${rank}/${allUser.length}
+├‣ 𝙼𝚘𝚗𝚎𝚢 𝚁𝚊𝚗𝚔: #${moneyRank}/${allUser.length}
+╰‣ 𝙱𝚊𝚋𝚢 𝚝𝚎𝚊𝚌𝚑: ${babyTeach || 0}`;
+const avatarStream = (await require("axios").get(avatarUrl, { responseType: "stream" })).data;
+   api.sendMessage({
+      body: userInformation,
+      attachment: avatarStream,
+    }, event.threadID, event.messageID);
+  },
 };
+
+function formatMoney(num) {
+  const units = ["", "K", "M", "B", "T", "Q", "Qi", "Sx", "Sp", "Oc", "N", "D"];
+  let unit = 0;
+  while (num >= 1000 && ++unit < units.length) num /= 1000;
+  return num.toFixed(1).replace(/\.0$/, "") + units[unit];
+}
