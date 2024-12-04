1fc0b3da486b2336be66956c335b1d8450fea103
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-12-04 07:50:02 +0600

    Update Profile.js

--- scripts/commands/Profile.js
+++ scripts/commands/Profile.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "profile", 
   version: "1.0.0", 
   permission: 0,
-  credits: "Nayan",
+  credits: "Nazrul",
   description: "example",
   prefix: true,
   category: "Fun", 
