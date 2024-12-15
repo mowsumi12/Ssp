23588d1042497a0e34b32b59a8631be1f82cecd4
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-12-15 19:27:02 +0600

    Update baby.js

--- scripts/commands/baby.js
+++ scripts/commands/baby.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const baseApiUrl = async () => {
-    const base = await axios.get(`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`);
+    const base = await axios.get(`https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan/main/api.json`);
     console.log(base.data.api);
     return base.data.api;
 };
